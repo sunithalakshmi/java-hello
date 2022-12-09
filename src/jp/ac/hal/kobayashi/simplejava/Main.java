085ea1e956492d7d0a5423582cdc3445b61edc14
sunithalakshmi <113410304+sunithalakshmi@users.noreply.github.com>
2022-12-09 17:07:49 +0530

    Update Main.java

--- src/jp/ac/hal/kobayashi/simplejava/Main.java
+++ src/jp/ac/hal/kobayashi/simplejava/Main.java
@@ -2,6 +2,6 @@ package jp.ac.hal.kobayashi.simplejava;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello java!");
 	}
 }
